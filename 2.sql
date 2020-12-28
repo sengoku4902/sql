ecda6f05751713bdb73a46ab5b13b7b1af3f6acf
sengoku4902 <sasazaki4902@gmail.com>
2020-12-28 22:11:53 +0900

    1228

--- 2.sql
+++ 2.sql
@@ -13,3 +13,41 @@ FROM
     items
 ORDER BY
     price DESC
+
+
+SELECT
+    itemname
+FROM
+    items
+ORDER BY
+    itemname ASC
+
+SELECT
+    itemname
+FROM
+    items
+ORDER BY
+    itemname DESC
+
+SELECT
+    billdate
+FROM
+    earnings
+ORDER BY
+    billdate ASC
+
+SELECT
+    billdate
+FROM
+    earnings
+ORDER BY
+    billdate DESC
+
+SELECT
+    itemcode,
+    price
+FROM
+    items
+ORDER BY
+    price DESC,
+    itemcode ASC
