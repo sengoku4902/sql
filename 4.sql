c34e13afbefeccf5001dbd7368f916db7647bcb9
sengoku4902 <sasazaki4902@gmail.com>
2020-12-31 23:37:41 +0900

    1231

---
+++ 4.sql
@@ -0,0 +1,20 @@
+SELECT
+    id,
+    itemcode,
+    price
+FROM
+    items
+LIMIT
+    30
+
+
+SELECT
+        id,
+        itemcode,
+        price
+    FROM
+        items
+    LIMIT
+        20
+    OFFSET
+        20
